2174b793801c38ce5fd609be5bae1ea8a6e03051
Mayank <ms772254@gmail.com>
2022-10-18 21:40:55 +0530

    a

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ document.querySelectorAll("imag")[1].setAttribute("src",randomImgSource2);
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 const randomNumber2 = Math.floor(Math.random() * 6) + 1;
  
-document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
-document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+document.querySelector(".img1").setAttribute("src", `dice${randomNumber1}.png`);
+document.querySelector(".img2").setAttribute("src", `dice${randomNumber2}.png`);
 
 
 if(randomNumber1>randomNumber2){
